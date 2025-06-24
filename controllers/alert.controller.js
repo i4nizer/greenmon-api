c05f02b97d238425e9486e76ccf922746496f64e
i4nizer <iansandoval264@gmail.com>
2025-06-24 22:56:00 +0800

    Added more query filters for alert GET request.

--- controllers/alert.controller.js
+++ controllers/alert.controller.js
@@ -1,3 +1,4 @@
+const { Op } = require("sequelize")
 const Alert = require("../models/alert.model")
 const { AppError } = require("../utils/app-error.util")
 
@@ -7,22 +8,42 @@ const { AppError } = require("../utils/app-error.util")
 const getAlert = async (req, res, next) => {
 	try {
 		const { userId } = req.accessTokenPayload
-		const { greenhouseId, startdt, enddt, limit } = req.query
+		const { greenhouseId, startdt, enddt, viewed, emailed, severity, offset, limit } = req.query
 
 		const filter = {
 			...(userId && { userId }),
 			...(greenhouseId && { greenhouseId }),
+			...(viewed && { viewed: viewed == 'true' }),
+			...(emailed && { emailed: emailed == 'true' }),
+			...(severity && { severity: { [Op.in]: severity?.split(',') } }),
 			...(startdt && { createdAt: { [Alert.sequelize.Op.gte]: new Date(startdt) } }),
 			...(enddt && { createdAt: { [Alert.sequelize.Op.lte]: new Date(enddt) } }),
 		}
 
+		// find them
 		const alertDocs = await Alert.findAll({
 			where: filter,
 			order: [["createdAt", "DESC"]],
 			...(limit && { limit: parseInt(limit, 10) }),
+			...(offset && { offset: parseInt(offset, 10) }),
 		})
 
-		res.json({ alerts: alertDocs.reverse() })
+		// send them
+		res.json({
+			count: await Alert.count({ where: filter }),
+			alerts: alertDocs.reverse(),
+		})
+
+		// mark them as viewed
+		await Alert.update(
+			{ viewed: true },
+			{
+				where: { id: { [Op.in]: alertDocs.map(a => a.id) } },
+				individualHooks: true,
+				source: 'client',
+			}
+		)
+
 	} catch (error) {
 		next(error)
 	}
--- validations/alert.validation.js
+++ validations/alert.validation.js
@@ -3,9 +3,12 @@ const Joi = require("joi")
 /** Requires greenhouseId or userId and optionally startdt, enddt, and limit. */
 const getAlertSchema = Joi.object({
 	greenhouseId: Joi.number().integer().optional(),
-	userId: Joi.number().integer().optional(),
+	viewed: Joi.boolean().optional(),
+	emailed: Joi.boolean().optional(),
+	severity: Joi.string().optional(),
 	startdt: Joi.date().iso().optional(),
 	enddt: Joi.date().iso().optional(),
+	offset: Joi.number().integer().optional(),
 	limit: Joi.number().integer().min(1).optional(),
 })
 
